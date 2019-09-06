fe81fed07abfe9f02ff40d63fafd2711e74ac110
Jesus Gonzalez Jaen <jesus.gonzalez.development@gmail.com>
2019-09-06 14:51:26 +0200

    project: skel

---
+++ frontend/src/pages/betslip.success/components/PageLayout.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { PageWrapper } from 'components'
+
+export const PageLayout = () => (
+)
---
+++ frontend/src/pages/betslip.success/containers/PageContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { PageLayout } from '../components'
+
+export const PageContainer = () => <PageLayout />
+
+PageContainer.propTypes = {}
