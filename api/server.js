f0be7478b93d8eaecfc1020b9a03bd97e0fdc47f
Jesus Gonzalez Jaen <jesus.gonzalez.development@gmail.com>
2019-09-06 16:38:43 +0200

    feature(api): filtering functions

--- api/server.js
+++ api/server.js
@@ -1,17 +1,38 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-app.use(cors());
+const { bets } = require('./data/data.json')
+
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.contentType('application/json')
+  next()
+})
 
 app.get('/decimalOddsMoreThanTwo', (req, res) => {
+  const betList = bets
+    .filter(bet => bet.odds.some(
+      ({ oddsDecimal }) => oddsDecimal >= 2
+    ))
 
-});
+  res
+    .status(200)
+    .end(JSON.stringify(betList))
+})
 
 app.get('/decimalOddsLessThanTwo', (req, res) => {
+  const betList = bets
+    .filter(bet => bet.odds.some(
+      ({ oddsDecimal }) => oddsDecimal < 2
+    ))
 
-});
+  res
+    .status(200)
+    .end(JSON.stringify(betList))
+})
 
 app.listen(4000, () => {
-  console.log('Example app listening on port 4000!');
-});
+  console.log('Example app listening on port 4000!')
+})
