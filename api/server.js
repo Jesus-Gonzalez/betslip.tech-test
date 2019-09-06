89d76fe6ad246a0579e77d8f913b774791e6b68d
Jesus Gonzalez Jaen <jesus.gonzalez.development@gmail.com>
2019-09-06 14:51:26 +0200

    project: skel

--- api/server.js
+++ api/server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/decimalOddsMoreThanTwo', (req, res) => {
-    // code here
+
 });
 
 app.get('/decimalOddsLessThanTwo', (req, res) => {
-    // code here
+
 });
 
 app.listen(4000, () => {
---
+++ frontend/src/pages/betslip.success/index.js
@@ -0,0 +1,1 @@
+export { PageContainer as BetslipSuccessPageContainer } from './containers'
