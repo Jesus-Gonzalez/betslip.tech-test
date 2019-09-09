e5f2eeed582930d6c2c64732e889ff790fcfcef2
Jesus Gonzalez <jesus.gonzalez.development@gmail.com>
2019-09-09 12:18:19 +0200

    feature(frontend/components/Dropdown/Arrow): reference image

--- frontend/src/components/Dropdown/components/DropdownTrigger/Arrow.jsx
+++ frontend/src/components/Dropdown/components/DropdownTrigger/Arrow.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
+import { downArrow as downArrowIcon } from 'assets/images'
+
 import styles from './styles.module.scss'
 
 const propTypes = {
@@ -20,7 +22,7 @@ export const Arrow = ({ className, isVisible, ...props }) => {
     <img
       alt='arrow icon'
       aria-expanded={isVisible}
-      src='/assets/images/down-arrow.svg'
+      src={downArrowIcon}
       className={arrowClass}
       {...props}
     />
