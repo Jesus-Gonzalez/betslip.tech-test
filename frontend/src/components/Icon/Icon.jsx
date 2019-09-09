1cd85a286a75d04379fef567d3e8f59da3416c0c
Jesus Gonzalez <jesus.gonzalez.development@gmail.com>
2019-09-09 21:32:42 +0200

    feature(components/Icon): initial version

---
+++ frontend/src/components/Icon/Icon.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import styles from './styles.module.scss'
+
+const propTypes = {
+  className: PropTypes.string,
+  country: PropTypes.string.isRequired
+}
+
+const defaultProps = {
+  className: ''
+}
+
+export const Icon = React.memo(({
+  className,
+  icon,
+  ...props
+}) => {
+  const imgClass = React.useMemo(
+    () => classnames(className, styles.icon, styles[icon], 'icon'),
+    [className, icon]
+  )
+
+  return (
+    <div
+      {...props}
+      className={imgClass}
+    />
+  )
+})
+
+Icon.propTypes = propTypes
+
+Icon.defaultProps = defaultProps
