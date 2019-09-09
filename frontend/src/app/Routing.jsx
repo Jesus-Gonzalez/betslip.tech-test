1b31c4e1247c398ed2e212fe043ff9895a28ddcf
Jesus Gonzalez <jesus.gonzalez.development@gmail.com>
2019-09-09 12:14:50 +0200

    fix(frontend/routing): 404 error and fix IndexRoute

--- frontend/src/app/Routing.jsx
+++ frontend/src/app/Routing.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 import * as Pages from 'pages'
 
 export const Routing = () => (
   <BrowserRouter>
-    <Route
-      path='/'
-      render={() => <Redirect to='/betslip' />}
-    />
-    <Route
-      path='/error'
-      component={Pages.Error}
-    />
-    <Route
-      exact
-      path='/betslip'
-      component={Pages.Betslip}
-    />
-    <Route
-      exact
-      path='/betslip/success'
-      component={Pages.BetslipSuccess}
-    />
-    <Route
-      exact
-      path='/betslip/error'
-      component={Pages.BetslipError}
-    />
-    <Route
-      path='/404'
-      component={Pages.NotFound}
-    />
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={() => <Redirect to='/betslip' />}
+      />
+      <Route
+        path='/error'
+        component={Pages.Error}
+      />
+      <Route
+        exact
+        path='/betslip'
+        component={Pages.Betslip}
+      />
+      <Route
+        exact
+        path='/betslip/success'
+        component={Pages.BetslipSuccess}
+      />
+      <Route
+        exact
+        path='/betslip/error'
+        component={Pages.BetslipError}
+      />
+      <Route
+        path='/404'
+        component={Pages.NotFound}
+      />
+      <Route
+        render={() => <Redirect to='/404' />}
+      />
+    </Switch>
   </BrowserRouter>
 )
