92137e5b997884ce90ea8c4ab6dd46c7c00f064c
Jesus Gonzalez <jesus.gonzalez.development@gmail.com>
2019-09-10 10:27:21 +0200

    fix(reducer/onSubmit): filter only valued stakes

--- frontend/src/store/reducers/betslip/lib/onSubmit.js
+++ frontend/src/store/reducers/betslip/lib/onSubmit.js
@@ -3,6 +3,7 @@ export const onSubmit = (state, { args: [values] }) => Object.assign(
   state,
   {
     totalStake: values.bet
+      .filter(stake => stake)
       .map(stake => +stake.replace(',', '.'))
       .reduce((total, stake) => total + stake, 0)
   }
